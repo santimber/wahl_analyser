f4e5068462a2d096ebe776ef224cb974a687e5fe
santimber <santiago.g.del@gmail.com>
2025-02-22 10:33:23 +0100

    update, remove old parts

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
 
-# Download NLTK data
-python post_install.py
-
-# Set NLTK data environment variable
-export NLTK_DATA=/opt/render/nltk_data
-
 # Start the web server with Gunicorn
 gunicorn app:app
